61669ad5b4a6047a38fdca8a1879d42907b75246
Kevin Kreed <kevin.fuccio00@gmail.com>
2023-01-14 17:33:39 +0100

    fixed navBar and image animation

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+let navBar = document.querySelector('.navBar');
+let navButton = document.querySelector('.navLinks button')
+window.onscroll = () =>{
+    if(window.scrollY > 466){
+        navBar.classList.add('navActive');
+        navButton.classList.add('navLinksBtn-Active')
+    }else {
+        navBar.classList.remove('navActive');
+        navButton.classList.remove('navLinksBtn-Active')
+    }
+};
+let MLetter = document.querySelectorAll(
+    "svg g[stroke-linecap='butt']"
+    );
+
+let intervallo = setInterval(() =>{
+    let randomNum = Math.floor(Math.random() * MLetter.length)
+    let randomM = MLetter[randomNum]
+    randomM.style.transition = '1s'
+    if(randomM.getAttribute("opacity") == 1){
+        randomM.setAttribute("opacity", "0")
+    }else{
+        randomM.setAttribute("opacity","1")
+    }
+},40)
+let stopIntervallo = () => {
+    clearInterval(intervallo)
+}
+
+
